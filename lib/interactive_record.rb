23e79c104b8ba551561560acbe9dd87de709ae7a
abattalov <abattalov14@gmail.com>
2021-11-13 19:07:40 -0600

    commit tree 854ceceec91c18cc038e3540e77969359587d6a6

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,77 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+
+    # Created a class method which defines a table name
+    # by taking the name of the class, converting it into a string
+    # then making it all lowercase and pluralizing it with the inflector gem.
+
+    def self.column_names
+        DB[:conn].results_as_hash = true
+
+        sql = "pragma table_info('#{table_name}')"
+
+        table_info = DB[:conn].execute(sql)
+        cloumn_names = []
+        table_info.each do |row|
+            cloumn_names << row["name"]
+        end
+        cloumn_names.compact
+    end
+
+    # Class method that retrieves column names from the db to convert into attr_accessors
+
+    def initialize(options={})
+        options.each do |property, value|
+            self.send("#{property}=", value)
+        end
+    end
+
+    #Self in the instance method bellow refers to an instance of the class and not class itself.
+
+    def table_name_for_insert
+        self.class.table_name
+    end
+
+    #SQL already makes a unique ID so we have to remove the "ID" portion from our column name method.
+    
+    def col_names_for_insert
+        self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    end
+
+    def values_for_insert
+        values = []
+        
+        self.class.column_names.each do |col_name|
+            values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end
+        values.join(", ")
+    end
+
+    def save
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES(#{values_for_insert})"
+
+        DB[:conn].execute(sql)
+
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM #{table_name} WHERE name = '#{name}'"
+        DB[:conn].execute(sql)
+    end
+
+    def self.find_by(attribute_hash)
+        value = attribute_hash.values.first
+        formatted_value = value.class == Fixnum ? value : "'#{value}'"
+        sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+        DB[:conn].execute(sql)
+    end
+
+    
   
 end
